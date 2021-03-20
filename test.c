d23455bf6c5998b5c9606ed378c12fee2a3bee13
Ananya Gupta <2020a1r139@mietjammu.in>
2021-03-20 21:53:39 +0530

    simple calculator using switch case and function

---
+++ test.c
@@ -0,0 +1,8 @@
+//a basic C program to print hello world.
+//start
+#include<stdio.h>	//pre processor directive to include standard input and output header file.
+int main(){	 //start of main function body.
+	printf("hello world");	//here we have entered the text to be printed.
+return 0;	//returning
+}
+//End

05c258463eaae53bd2635a6f74228bed35b2957d
Ananya Gupta <2020a1r139@mietjammu.in>
2021-03-20 16:52:11 +0530

    sum program

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+// A basic C program to print hello world.
+//start
+#include <stdio.h>//Including standard input output header files from library.
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+   printf("Hello World!");//here we have entered the text to be printed.
+   return 0;//returning.
 }
+//end

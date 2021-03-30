79bc9b01dffb825958091746f732566caaeed8e2
ananya170 <76464879+ananya170@users.noreply.github.com>
2021-03-30 22:38:47 +0530

    Update 10_area.c

--- 10_area.c
+++ 10_area.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-#include <math.h>
-int main(){
+//A C Program to calculate area of circle, square and rectangle.
+//area of circle : 22/7*r*r
+//area of square : r*r
+//area of rectangle : length*breadth
+
+#include <stdio.h>	//pre-processor directive of standard input output header file.
+#include <math.h>	
+int main(){	//start of main function body.
 	char purpose;
 	printf("Enter the mode of operation.\n 1.Area of Circle.\n 2.Area of Square.\n 3.Area of Rectangle.\n");
 	scanf("%c", &purpose);
 	float value1, value2;
 	printf("Enter values of the shape:\n");
 	scanf("%f %f", &value1, &value2);
-	switch(purpose)
+	switch(purpose)		//switch function used
 	{
 		case'1':
 			printf("Area of Circle = %f", ((22*value1*value1)/7));
